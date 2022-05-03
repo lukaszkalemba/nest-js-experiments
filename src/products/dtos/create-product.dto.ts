5753b65fe57ce081c8a9ef03c73050584c90f8b7
Lukasz <lukasz@Matrixs-MacBook-Pro.local>
2022-05-03 10:34:15 +0200

    Use validators

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
   title: string;
+
+  @IsString()
   price: number;
 }
--- src/products/dtos/update-product.dto.ts
+++ src/products/dtos/update-product.dto.ts
@@ -1,4 +1,11 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class UpdateProductDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsNumber({}, { message: 'Price must be a number' })
+  @IsOptional()
   price?: number;
 }
